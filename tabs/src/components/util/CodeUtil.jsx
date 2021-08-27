1ddad3b328107e74f496f0ba3d641777eb1aa822
Ji <t-lji@microsoft.com>
2021-08-27 10:49:15 +0800

    feat: change copy code icon
    Change the icon instead of popping up an alert window to notify the successful copy code

--- tabs/src/components/util/CodeUtil.jsx
+++ tabs/src/components/util/CodeUtil.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Provider, teamsTheme } from "@fluentui/react-northstar";
-import { ClipboardCopiedToIcon } from '@fluentui/react-icons-northstar'
+import { AcceptIcon, ClipboardCopiedToIcon } from '@fluentui/react-icons-northstar'
 import copy from 'copy-to-clipboard';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -23,16 +23,16 @@ function Highlight(props) {
 
 // provide copy code components
 export default function Code(props) {
+  const [isCopied, setIsCopied] = React.useState(false);
+
   return (
     <div>
-      <Button icon={ <ClipboardCopiedToIcon /> } content="click to copy code" 
-        onClick={ () => {
-          if (copy(props.code)) {
-            alert('success')
-          } else {
-            alert('error, please copy with your mouse');
-          }
-        }} />
+      <Button 
+        icon={ isCopied ? <AcceptIcon /> : <ClipboardCopiedToIcon /> } 
+        content="click to copy code" 
+        onClick={ () => { copy(props.code) ? 
+          setIsCopied(true) || setTimeout(() => { setIsCopied(false) }, 1000) : 
+          alert('error, please copy with your mouse or keyboard') }} />
       <Highlight>{ props.code }</Highlight>
     </div>
   )
