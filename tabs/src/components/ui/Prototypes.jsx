59e1a849b6f7221d32694b28e9f21a5bbc94ec1e
Ji Li <t-lji@microsoft.com>
2021-10-02 21:25:41 +0800

    feat(tabs/ui/prototypes/toolbars): add ToolBars page

--- tabs/src/components/ui/Prototypes.jsx
+++ tabs/src/components/ui/Prototypes.jsx
@@ -5,6 +5,7 @@ import Chat from './prototypes/Chat.jsx'
 import LayOut from './prototypes/LayOut.jsx'
 import Texts from './prototypes/Texts.jsx'
 import Users from './prototypes/Users.jsx'
+import ToolBars from './prototypes/ToolBars.jsx'
 import { TopJumper, ScrollToAnchor } from "../util/ScrollUtil";
 
 export default class Prototypes extends React.Component {
@@ -17,12 +18,13 @@ export default class Prototypes extends React.Component {
           <li><Button onClick={ () => ScrollToAnchor('LayOut') }>LayOut</Button></li>
           <li><Button onClick={ () => ScrollToAnchor('Texts') }>Texts</Button></li>
           <li><Button onClick={ () => ScrollToAnchor('Users') }>Users</Button></li>
-          <li><Button onClick={ () => ScrollToAnchor('ToolBar') }>ToolBar</Button></li>
+          <li><Button onClick={ () => ScrollToAnchor('ToolBars') }>ToolBars</Button></li>
         </ul>
         <div id="Chat"><Chat /></div>
         <div id="LayOut"><LayOut /></div>
         <div id="Texts"><Texts /></div>
         <div id="Users"><Users /></div>
+        <div id="ToolBars"><ToolBars /></div>
       <TopJumper />
     </div>
     )
