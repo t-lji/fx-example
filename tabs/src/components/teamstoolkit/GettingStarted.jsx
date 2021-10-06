956b48ee74f682da95f545b040771c84ab6ecb2f
Ji Li <t-lji@microsoft.com>
2021-10-06 15:20:03 +0800

    refactor(tabs/teamstoolkit): new ui design

--- tabs/src/components/teamstoolkit/GettingStarted.jsx
+++ tabs/src/components/teamstoolkit/GettingStarted.jsx
@@ -1,46 +1,246 @@
 import React from "react";
-import { Box, Button, Image, Text } from "@fluentui/react-northstar";
+import { Box, Button, Flex, Image, Text, SplitButton } from "@fluentui/react-northstar";
 import CreateTableSQL from '!!raw-loader!../../assets/code/teamsToolkit/gettingStarted/createTableSQL.sql'
+import { TopJumper, ScrollToAnchor } from "../util/ScrollUtil";
 import Code from "../util/CodeUtil"
+import { ComponentPrototype } from "../util/PageUtil.jsx";
 
 export default class GettingStarted extends React.Component {
   render() {
+    const menu = [
+      {
+        key: 'Download Sample',
+        content: '1. Download Sample',
+      },
+      {
+        key: 'Provision in the Cloud',
+        content: '2. Provision in the Cloud',
+      },
+      {
+        key: 'Deploy to the Cloud',
+        content: '3. Deploy to the Cloud',
+      },
+      {
+        key: 'Find your database',
+        content: '4. Find your database',
+      },
+      {
+        key: 'Log in your database',
+        content: '5. Log in your database',
+      },
+      {
+        key: 'Create database in Azure',
+        content: '6. Create database in Azure',
+      },
+      {
+        key: 'Run your app',
+        content: '7. Run your app',
+      },
+      {
+        key: 'Select scope of your app',
+        content: '8. Select scope of your app',
+      },
+      {
+        key: 'Set up your app for your selected scope',
+        content: '9. Set up your app for your selected scope',
+      },
+      {
+        key: 'Confirm your tab configuration for your selected scope',
+        content: '10. Confirm your tab configuration for your selected scope',
+      },
+      {
+        key: 'Enter the app for the first time',
+        content: '11. Enter the app for the first time',
+      },
+      {
+        key: 'Accept permission for use your profile',
+        content: '12. Accept permission for use your profile',
+      },
+      {
+        key: 'Enjoy your first app in Teams',
+        content: '13. Enjoy your first app in Teams',
+      },
+    ]
     return (
-    <Box>
-      <h1>Getting Started</h1>
-      <h2>Download Sample</h2>
-      <Image src="pic/getting_start_download_sample.gif" />
-      <h2>Provision in the Cloud</h2>
-      <Image src="pic/getting_start_provision.gif" />
-      <h2>Deploy to the Cloud</h2>
-      <Image src="pic/getting_start_deploy.gif" />
-      <h2>Find your database</h2>
-      <Text>Open <strong>.fx/env.default.json</strong> file, you could get the database name in <strong>databaseName</strong> setting</Text>
-      <Text>In <Button onClick={ event => { window.open("https://ms.portal.azure.com/", "_blank"); } }>Azure portal</Button>, find the database and use query editor</Text>
-      <Image src="pic/getting_start_all_resources.png" />
-      <h2>Log in your database</h2>
-      <Text>Use your Login and Password in step 2</Text>
-      <Image src="pic/getting_start_all_resources_login.png" />
-      <h2>Create database in Azure</h2>
-      <Text>With below query to create a table</Text>
-      <Code code={ CreateTableSQL }></Code>
-      <Image src="pic/getting_start_all_resources_create_table.png" />
-      <h2>Run your app</h2>
-      <Text>Open Debug View <strong>(Ctrl+Shift+D)</strong>, select <strong>Launch Remote (Edge)</strong>, then Click <strong>start</strong> or Press <strong>F5</strong></Text>
-      <Image src="pic/getting_start_run.png" />
-      <h2>Select scope of your app</h2>
-      <Image src="pic/getting_start_select_scope.png" />
-      <h2>Set up your app for your selected scope</h2>
-      <Image src="pic/getting_start_set_up.png" />
-      <h2>Confirm your tab configuration for your selected scope</h2>
-      <Image src="pic/getting_start_tab_configuration.png" />
-      <h2>Enter the app for the first time</h2>
-      <Image src="pic/getting_start_start_page.png" />
-      <h2>Accept permission for use your profile</h2>
-      <Image src="pic/getting_start_permission_page.png" />
-      <h2>Enjoy your first app in Teams</h2>
-      <Image src="pic/getting_start_result.gif" />
-    </Box>
+      <Box className="Getting Started page">
+        <Flex gap="gap.small">
+          <Box class="HeaderBox">
+            <Box class="HeaderContent">
+              <SplitButton
+                menu={ menu }
+                button={{
+                  content: 'Getting Started',
+                  'aria-roledescription': 'splitbutton',
+                  'aria-describedby': 'instruction-message-primary-button',
+                }}
+                primary
+                size="largest"
+                toggleButton={{
+                  'aria-label': 'more options',
+                }}
+                onMenuItemClick= { (e, { index }) => ScrollToAnchor(menu[index].key) }
+              />
+            </Box>
+          </Box>
+          <Box class="ContentBox">
+            <ComponentPrototype 
+              title="Getting Started"
+              description="Get Started with your first Teams App!"
+            >
+              <Flex column>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='1. Download Sample:' 
+                    id="Download Sample"
+                  />
+                  </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_download_sample.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='2. Provision in the Cloud:' 
+                    id="Provision in the Cloud"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_provision.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='3. Deploy to the Cloud:' 
+                    id="Deploy to the Cloud"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_deploy.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='4. Find your database:' 
+                    id="Find your database"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>Open <strong>.fx/env.default.json</strong> file, you could 
+                    get the database name in <strong>databaseName</strong> setting
+                  </Text>
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>In  
+                    <Button 
+                      onClick={ event => { window.open("https://ms.portal.azure.com/", "_blank"); } }
+                      content="Azure portal"
+                    />
+                    , find the database and use query editor
+                  </Text>
+                </Flex>
+                <Flex class="StepContent">
+                  <Image src="pic/getting_start_all_resources.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='5. Log in your database:' 
+                    id="Log in your database"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>Use your Login and Password in 
+                    <Button 
+                      onClick={ event => { ScrollToAnchor("Provision in the Cloud") } }
+                      content="step 2" 
+                    />
+                  </Text>
+                </Flex>
+                <Flex class="StepContent">
+                  <Image src="pic/getting_start_all_resources_login.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='6. Create database in Azure:' 
+                    id="Create database in Azure"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>With below query to create a table</Text>
+                  <Code code={ CreateTableSQL }></Code>
+                  <Image fluid src="pic/getting_start_all_resources_create_table.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='7. Run your app:' 
+                    id="Run your app"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>Open Debug View (<code>Ctrl+Shift+D</code>), select 
+                    <strong>Launch Remote (Edge)</strong>, then Click 
+                    <strong> start</strong> or Press <code>F5</code>.
+                  </Text>
+                </Flex>
+                <Flex class="StepContent">
+                  <Image src="pic/getting_start_run.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='8. Select scope of your app:' 
+                    id="Select scope of your app"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_select_scope.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='9. Set up your app for your selected scope:' 
+                    id="Set up your app for your selected scope"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_set_up.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='10. Confirm your tab configuration for your selected scope:'
+                    id="Confirm your tab configuration for your selected scope" 
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image src="pic/getting_start_tab_configuration.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='11. Enter the app for the first time:' 
+                    id="Enter the app for the first time"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_start_page.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='12. Accept permission for use your profile:' 
+                    id="Accept permission for use your profile"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image src="pic/getting_start_permission_page.png" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='13. Enjoy your first app in Teams:' 
+                    id="Enjoy your first app in Teams"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/getting_start_result.gif" />
+                </Flex>
+              </Flex>
+            </ComponentPrototype>
+            <TopJumper />
+          </Box>
+        </Flex>
+      </Box>
     )
   }
 }
--- tabs/src/components/teamstoolkit/Project.jsx
+++ tabs/src/components/teamstoolkit/Project.jsx
@@ -1,25 +1,119 @@
 import React from "react";
-import { Box, Image, Text } from "@fluentui/react-northstar";
+import { Box, Flex, Image, Text, SplitButton } from "@fluentui/react-northstar";
+import { TopJumper, ScrollToAnchor } from "../util/ScrollUtil";
+import { ComponentPrototype } from "../util/PageUtil.jsx";
 
 class Project extends React.Component {
   render() {
+    const menu = [
+      {
+        key: 'Create New Project',
+        content: '1. Create New Project',
+      },
+      {
+        key: 'Provision in the Cloud',
+        content: '2. Provision in the Cloud',
+      },
+      {
+        key: 'Deploy to the Cloud',
+        content: '3. Deploy to the Cloud',
+      },
+      {
+        key: 'Local Debug',
+        content: '4. Local Debug',
+      },
+      {
+        key: 'Add Capabilities',
+        content: '5. Add Capabilities',
+      },
+    ]
     return (
-    <Box>
-      <h1>Project</h1>
-      <h2>Create New Project</h2>
-      <Image src="pic/create_new_project.gif" />
-      <h2>Provision in the Cloud</h2>
-      <Image src="pic/provision_in_the_cloud.gif" />
-      <h2>Deploy to the Cloud</h2>
-      <Image src="pic/deploy_to_the_cloud.gif" />
-      <h2>Local Debug</h2>
-      <Text>Or just press <strong>F5</strong></Text>
-      <Image src="pic/local_dubug.gif" />
-      <Text>Local Dubug Page</Text>
-      <Image src="pic/local_dubug_web_page.gif" />
-      <h2>Add Capabilities</h2>
-      <Image src="pic/add_capabilities.gif" />
-    </Box>
+      <Box className="Project page">
+        <Flex gap="gap.small">
+          <Box class="HeaderBox">
+            <Box class="HeaderContent">
+              <SplitButton
+                menu={ menu }
+                button={{
+                  content: 'Project',
+                  'aria-roledescription': 'splitbutton',
+                  'aria-describedby': 'instruction-message-primary-button',
+                }}
+                primary
+                size="largest"
+                toggleButton={{
+                  'aria-label': 'more options',
+                }}
+                onMenuItemClick= { (e, { index }) => ScrollToAnchor(menu[index].key) }
+              />
+            </Box>
+          </Box>
+          <Box class="ContentBox">
+            <ComponentPrototype 
+              title="Project"
+              description="Create your first Project by Teams Toolkit!"
+            >
+              <Flex column>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='1. Create New Project:' 
+                    id="Create New Project"
+                  />
+                  </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/create_new_project.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='2. Provision in the Cloud:' 
+                    id="Provision in the Cloud"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/provision_in_the_cloud.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='3. Deploy to the Cloud:' 
+                    id="Deploy to the Cloud"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/deploy_to_the_cloud.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='4. Local Debug:' 
+                    id="Local Debug"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>Or just press <code>F5</code></Text>
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/local_dubug.gif" />
+                </Flex>
+                <Flex class="StepContent">
+                  <Text>Local Dubug Page</Text>
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/local_dubug_web_page.gif" />
+                </Flex>
+                <Flex>
+                  <Text weight="regular" size="large" 
+                    content='5. Add Capabilities:' 
+                    id="Add Capabilities"
+                  />
+                </Flex>
+                <Flex class="StepContent">
+                  <Image fluid src="pic/add_capabilities.gif" />
+                </Flex>
+              </Flex>
+            </ComponentPrototype>
+            <TopJumper />
+          </Box>
+        </Flex>
+      </Box>
     )
   }
 }
